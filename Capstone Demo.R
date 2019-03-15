7bc127bff2ebea75f40be253f18a18d4de552254
bengalassi <35591271+bengalassi@users.noreply.github.com>
2019-03-15 04:14:04 -0700

    Add files via upload
    Added cool new stuff and made it pretty

--- Capstone Demo.R
+++ Capstone Demo.R
@@ -6,39 +6,67 @@ library(udpipe)
 library(tidytext)
 library(sentimentr)
 library(DT)
+library(wordcloud)
 
 
 ui <- fluidPage(
-  titlePanel("Vooey Wizard"),
-  #img(src="doNotOpen.png"),
-  textInput("text_entry","Enter dialogue: ",width="500px"),
-  h4("Word Frequency"),
-  tableOutput("freq_table"),
-  radioButtons("pos", "Filter by Part of Speech:",
-               c("All" = "pos_all",
-                 "Nouns" = "pos_nouns",
-                 "Adjectives" = "pos_adjectives",
-                 "Verbs" = "pos_verbs")),
-  h4("Nouns"),
-  verbatimTextOutput("pos_debug1"),
-  h4("Verbs"),
-  verbatimTextOutput("pos_debug2"),
-  h4("Adjectives"),
-  verbatimTextOutput("pos_debug3"),
-  h4("Sentence Sentiment"),
-  dataTableOutput("sent_table"),
-  h4("Filtered by Keyword Table"),
-  textInput("keyword","Enter keyword: ",width="500px"),
-  tableOutput("filtered_by_keyword_table")
+  theme="readable",
+  sidebarLayout(
+    sidebarPanel(
+      img(src="wizard.png",width="50%"),
+      titlePanel("Vooey Wizard"),
+      textInput("vooeys_question","Enter Vooey's question:",width="100%"),
+      textAreaInput("text_entry","Enter dialogue: ",width="100%",height="200px")
+    ),
+    mainPanel(
+      tabsetPanel(
+        tabPanel(
+          "Word Frequency",
+          h2("Word Frequency"),
+          plotOutput("freq_cloud"),
+          radioButtons("pos", "Filter by:",
+                       c("All" = "pos_all",
+                         "Nouns" = "pos_nouns",
+                         "Adjectives" = "pos_adjectives",
+                         "Verbs" = "pos_verbs")),
+          dataTableOutput("freq_table")
+        ),
+        tabPanel(
+          "Parts of Speech",
+          h2("Parts of Speech"),
+          h4("Nouns"),
+          verbatimTextOutput("pos_debug1"),
+          h4("Verbs"),
+          verbatimTextOutput("pos_debug2"),
+          h4("Adjectives"),
+          verbatimTextOutput("pos_debug3")
+        ),
+        tabPanel(
+          "Sentiment",
+          h2("Sentence Sentiment"),
+          dataTableOutput("sent_table"),
+          h4("Filtered by Keyword Table"),
+          textInput("keyword","Enter keyword: ",width="500px"),
+          tableOutput("filtered_by_keyword_table")
+        ),
+        tabPanel(
+          "N-Grams",
+          textInput("cooccurring_term",label="Enter term:"),
+          tableOutput("cooccurance_matrix")
+        )
+      )
+    )
+  )
 )
 
 server <- function(input,output){
   
   udmodel_download <- udpipe_download_model(language = "english")
   udmodel <- udpipe_load_model(file = udmodel_download$file_model)
-  
+
   tagged_pos_table <- reactive({
-    x <- udpipe_annotate(udmodel,unlist(strsplit(input$text_entry, "(?<=[[:punct:]])\\s(?=[A-Z])", perl=T)))
+    text_entry_character_vector <- unlist(strsplit(input$text_entry, "(?<=[[:punct:]])\\s(?=[A-Z])", perl=T))
+    x <- udpipe_annotate(udmodel,text_entry_character_vector)
     x_as_frame <- as.data.frame(x)
     pos_tagged_words_table <- x_as_frame %>% 
       select(token,upos)
@@ -49,23 +77,22 @@ server <- function(input,output){
       nouns_only <- tagged_pos_table() %>% 
         filter(upos=="NOUN") %>%
         select(token)
-    return(nouns_only)
+    return(unique(nouns_only$token))
   })
   
   filter_by_pos_verbs <- reactive({
     verbs_only <- tagged_pos_table() %>% 
       filter(upos=="VERB") %>%
       select(token)
-    return(verbs_only)
+    return(unique(verbs_only$token))
   })
   
   filter_by_pos_adjectives <- reactive({
     adjectives_only <- tagged_pos_table() %>% 
       filter(upos=="ADJ") %>%
       select(token)
-    return(adjectives_only)
+    return(unique(adjectives_only$token))
   })
-  
   
   calculate_frequency <- reactive({
     themes_source <- VectorSource(input$text_entry)
@@ -80,33 +107,68 @@ server <- function(input,output){
     term_frequency <- rowSums(themes_m)
     term_frequency <- sort(term_frequency,decreasing = TRUE)
     term <- names(term_frequency)
-    if(input$pos == "pos_all"){
-      freq_frame <- data.frame(term,term_frequency)
-    }
-    
-    if(input$pos == "pos_nouns"){
-      freq_frame <- data.frame(term,term_frequency) %>%
+    freq_frame <- data.frame(input$vooeys_question,term,term_frequency)
+    if(input$pos=="pos_nouns"){
+      freq_frame <- data.frame(input$vooeys_question,term,term_frequency)
+      freq_frame <- freq_frame %>% 
         filter(term %in% filter_by_pos_nouns())
     }
-    
-    if(input$pos == "pos_verbs"){
-      freq_frame <- data.frame(term,term_frequency) %>%
+    if(input$pos=="pos_verbs"){
+      freq_frame <- data.frame(input$vooeys_question,term,term_frequency)
+      freq_frame <- freq_frame %>% 
         filter(term %in% filter_by_pos_verbs())
     }
-    
-    if(input$pos == "pos_adjectives"){
-      freq_frame <- data.frame(term,term_frequency) %>%
+    if(input$pos=="pos_adjectives"){
+      freq_frame <- data.frame(input$vooeys_question,term,term_frequency)
+      freq_frame <- freq_frame %>% 
         filter(term %in% filter_by_pos_adjectives())
     }
-    
+    names(freq_frame) <- c("question","term","term_frequency")
+    freq_frame <- datatable(freq_frame, options = list(searchHighlight = TRUE),rownames=FALSE)
     return(freq_frame)
     })
   
+  create_wordcloud <- reactive({
+    themes_source <- VectorSource(input$text_entry)
+    themes_corpus <- VCorpus(themes_source)
+    other_words_to_remove <- c()
+    corpus <- tm_map(themes_corpus,removePunctuation)
+    corpus <- tm_map(corpus, content_transformer(tolower))
+    corpus <- tm_map(corpus, removeWords, c(stopwords("en"),other_words_to_remove))
+    cleaned_themes_corpus <- corpus
+    themes_tdm <- TermDocumentMatrix(cleaned_themes_corpus)
+    themes_m <- as.matrix(themes_tdm)
+    term_frequency <- rowSums(themes_m)
+    term_frequency <- sort(term_frequency,decreasing = TRUE)
+    term <- names(term_frequency)
+    freq_frame <- data.frame(input$vooeys_question,term,term_frequency)
+    if(input$pos=="pos_nouns"){
+      freq_frame <- data.frame(input$vooeys_question,term,term_frequency)
+      freq_frame <- freq_frame %>% 
+        filter(term %in% filter_by_pos_nouns())
+    }
+    if(input$pos=="pos_verbs"){
+      freq_frame <- data.frame(input$vooeys_question,term,term_frequency)
+      freq_frame <- freq_frame %>% 
+        filter(term %in% filter_by_pos_verbs())
+    }
+    if(input$pos=="pos_adjectives"){
+      freq_frame <- data.frame(input$vooeys_question,term,term_frequency)
+      freq_frame <- freq_frame %>% 
+        filter(term %in% filter_by_pos_adjectives())
+    }
+    cloud <- wordcloud(words = freq_frame$term, freq = freq_frame$term_frequency, min.freq = 1,
+              max.words=200, random.order=FALSE, rot.per=0.35)
+    return(cloud)
+  })
+
   calculate_sentiment <- reactive({
-    sentences <- get_sentences(input$text_entry)
+    sentences <- get_sentences(tolower(input$text_entry))
+    #gsub('[[:punct:] ]+',' ',sentences)
     sentence_sentiment <- sentiment(sentences)
-    sent_frame <- data.frame(sentences,sentence_sentiment$sentiment)
-    names(sent_frame) <- c("sentence","sentiment")
+    sent_frame <- data.frame(input$vooeys_question,sentences,sentence_sentiment$sentiment)
+    names(sent_frame) <- c("question","sentence","sentiment")
+    sent_frame <- datatable(sent_frame, options = list(searchHighlight = TRUE),rownames=FALSE)
     return(sent_frame)
   })
   
@@ -117,12 +179,19 @@ server <- function(input,output){
     return(filtered_by_keyword_table)
   })
   
-  output$freq_table <- renderTable(calculate_frequency())
+  calculate_cooccurrence <- reactive({
+      #Finish this
+  })
+  
+  
+  output$freq_table <- renderDataTable(calculate_frequency())
+  output$freq_cloud <- renderPlot(create_wordcloud())
   output$sent_table <- renderDataTable(calculate_sentiment())
   output$filtered_by_keyword_table <- renderTable(filter_by_keyword())
   output$pos_debug1 <- renderPrint(filter_by_pos_nouns())
   output$pos_debug2 <- renderPrint(filter_by_pos_verbs())
   output$pos_debug3 <- renderPrint(filter_by_pos_adjectives())
+  output$cooccurance_matrix <- renderTable(calculate_cooccurrence())
 }
 
 
